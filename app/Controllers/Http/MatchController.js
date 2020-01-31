cf688f059c99e7de0cd96d7555da1a3373df8c2f
jual ahmed <md.jual.ah@gmail.com>
2020-01-31 11:01:38 +0600

    added match

--- app/Controllers/Http/MatchController.js
+++ app/Controllers/Http/MatchController.js
@@ -9,89 +9,93 @@
  */
 const { validate } = use('Validator');
 const Match = use('App/Models/Match')
+const Product = use('App/Models/Product')
+const Maps = use('App/Models/Map')
 class MatchController {
-  /**
-   * Show a list of all matches.
-   * GET matches
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-    const match = await Match.all();
-    return view.render('setup/match/index',{matches: match.rows});
-  }
+	/**
+	 * Show a list of all matches.
+	 * GET matches
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async index ({ request, response, view }) {
+		const match = await Match.all();
+		const product = await Product.all();
+		const maps = await Maps.all();
+		return view.render('setup/match/index',{maps: maps.rows},{products: product.rows},{match: match.rows});
+	}
 
-  /**
-   * Render a form to be used for creating a new match.
-   * GET matches/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+	/**
+	 * Render a form to be used for creating a new match.
+	 * GET matches/create
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async create ({ request, response, view }) {
+	}
 
-  /**
-   * Create/save a new match.
-   * POST matches
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
+	/**
+	 * Create/save a new match.
+	 * POST matches
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async store ({ request, response }) {
+	}
 
-  /**
-   * Display a single match.
-   * GET matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
+	/**
+	 * Display a single match.
+	 * GET matches/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async show ({ params, request, response, view }) {
+	}
 
-  /**
-   * Render a form to update an existing match.
-   * GET matches/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+	/**
+	 * Render a form to update an existing match.
+	 * GET matches/:id/edit
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async edit ({ params, request, response, view }) {
+	}
 
-  /**
-   * Update match details.
-   * PUT or PATCH matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
+	/**
+	 * Update match details.
+	 * PUT or PATCH matches/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async update ({ params, request, response }) {
+	}
 
-  /**
-   * Delete a match with id.
-   * DELETE matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
-  }
+	/**
+	 * Delete a match with id.
+	 * DELETE matches/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async destroy ({ params, request, response }) {
+	}
 }
 
 module.exports = MatchController

7553f4ab5f1726dbba736fdcb6ebe962e3ba0420
jual ahmed <md.jual.ah@gmail.com>
2020-02-27 00:56:45 +0600

    ab

--- app/Controllers/Http/TransactionController.js
+++ app/Controllers/Http/TransactionController.js
@@ -4,6 +4,7 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 const Transaction = use('App/Models/Transaction');
+const User = use('App/Models/User');
 const Database = use('Database')
 /**
  * Resourceful controller for interacting with transactions
@@ -36,65 +37,75 @@ class TransactionController {
 	}
 
 
-  async show ({ request, response, view }) {
-    const transaction = await Transaction.all();
-    return view.render('Setup/transaction/index',
+	async show ({ request, response, view }) {
+		const transaction = await Transaction.all();
+		return view.render('Setup/transaction/index',
 			{
-			    transactions: transaction.rows,
+				transactions: transaction.rows,
 			}
 		);
-  }
+	}
 
-  /**
-   * Create/save a new transaction.
-   * POST transactions
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response, view }) {
-    const transaction = await Database.table('transactions').orderBy('id', 'desc');
-    // return transaction;
-    return view.render('Setup/transaction/index',
+	/**
+	 * Create/save a new transaction.
+	 * POST transactions
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async store ({ request, response, view }) {
+		const transaction = await Database.table('transactions').orderBy('id', 'desc');
+		// return transaction;
+		return view.render('Setup/transaction/index',
 			{
-			    transactions: transaction,
+				transactions: transaction,
 			}
 		);
-  }
+	}
 
-  /**
-   * Render a form to update an existing transaction.
-   * GET transactions/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-    const id = params.id;
+	/**
+	 * Render a form to update an existing transaction.
+	 * GET transactions/:id/edit
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async edit ({ params, request, response, view }) {
+		const id = params.id;
 		// Console.log(id);
 		const transaction = await Transaction.find(id);
 		return transaction;
-  }
+	}
 
-  /**
-   * Update transaction details.
-   * PUT or PATCH transactions/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-    const id = await params.id;
-    const transaction = await Transaction.find(id);
-    transaction.status = request.input('status')
-
-    await transaction.save();
+	/**
+	 * Update transaction details.
+	 * PUT or PATCH transactions/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async update ({ params, request, response }) {
+		const id = await params.id;
+		const transaction = await Transaction.find(id);
+		var status = request.input('status')
+		if(status=='completed' && transaction.purpose=='addwallet'){
+			let user = await User.find(transaction.user_id);
+			user.wallet=user.wallet+transaction.amount
+			await user.save();
+		}
+		if(status=='completed' && transaction.purpose=='withdraw'){
+			let user = await User.find(transaction.user_id);
+			user.earn_wallet=user.earn_wallet-transaction.amount
+			await user.save();
+		}
+		transaction.status=status;
+		await transaction.save();
 		return response.redirect('/transaction');
-  }
+	}
 	
 }
 

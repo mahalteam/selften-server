a2bc102e38e7e88400fae6497fdf7e09f0b1d0c9
jual ahmed <md.jual.ah@gmail.com>
2020-03-01 09:14:03 +0600

    auto password Hase with auth update

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,11 +1,18 @@
 'use strict'
-
+const Hash = use('Hash')
 class HomeController {
     index ({ view }) {
         return view.render("index");
     }
     table ({view}) {
         return view.render("table");
+    }
+
+    async hastext({ params, view }){
+    	let savepass = await Hash.make('123456')
+
+    	const isSame = await Hash.verify('123456', savepass)
+    	return isSame;
     }
 }
 

d86c0e1756b9ac065081118515eef4ce48643d43
jual ahmed <md.jual.ah@gmail.com>
2020-03-25 14:00:46 +0600

    fatche indivisul user order

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -124,7 +124,7 @@ class OrderController {
 	 * @param {View} ctx.view
 	 */
 	async show ({ params, request, response, view }) {
-		const ddd = await Order.query().where('user_id',user_id).limit(50).fetch();
+		const ddd = await Order.query().where('user_id',params.id).limit(50).fetch();
 		return ddd;
 	}
 

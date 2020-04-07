febf6e780cfa1a069558e5b8807b94177eb65e9d
jual ahmed <md.jual.ah@gmail.com>
2020-04-07 20:51:13 +0600

    df

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -98,7 +98,7 @@ class OrderController {
 
 		let user_id= request.input('user_id')
 		let amount= request.input('amount')
-		const ddd = await Eventorder.query().where('user_id',user_id).where('active',1).orWhere('active',2).getCount();
+		const ddd = await Eventorder.query().where('user_id',user_id).where('active',1).getCount();
 		if(ddd>0){
 			response.json('You Have Already A Pending Order. Please Completed To Add Another Order');
 		}else{
